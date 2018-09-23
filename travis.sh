090ffd1c7cdffa50d544c2d079cfeca6183dd9ef
scoder <stefan_ml@behnel.de>
2018-09-23 22:50:05 +0200

    Update travis config to point to Cython at b6509bf791bfe44abbbaa957ae530e5910815dfd.

--- travis.sh
+++ travis.sh
@@ -17,7 +17,7 @@ fi
 
 if [ "${VIA}" == "compile" ]; then
   pip install -r requirements.txt
-  pip install --install-option="--no-cython-compile" https://github.com/cython/cython/archive/7b4079c50d88f5ce1cf3cc55a6f46cd6e35cbda4.zip
+  pip install --install-option="--no-cython-compile" https://github.com/cython/cython/archive/b6509bf791bfe44abbbaa957ae530e5910815dfd.zip
   python setup.py build_ext --inplace
   pip install -e .
 fi
